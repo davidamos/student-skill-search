fe00c694507af8bc3e6c15f9a18b7ddf7df21dd2
kmert10 <mertkarakas1997@gmail.com>
2018-09-20 18:06:55 -0400

    Added models to the models.py and set up docker-compose.yml

--- app/mmk2nh_cs4501/helloworld/models.py
+++ app/mmk2nh_cs4501/helloworld/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+class Shoe(models.Model):
+	index = models.IntegerField(default=-1) # index number, uniqueID
+	name = models.CharField(max_length=None) # Name of shoe
+	typeID = models.IntegerField(default=-1) # shoe type ID
+	brand = models.CharField(max_length=None) # Name of brand
+	price = models.DecimalField(default=-1.0, decimal_places=2)
+	tradable = models.IntegerField(default=-1) # tradable flag
+	sellable = models.IntegerField(default=-1) # sellable flag
+	userID = models.IntegerField(default=-1) # userID -> sellers table
+
+class User(models.Model):
+	index = models.IntegerField(default=-1) # sellerID in 'Shoes' correspond to index number, uniqueID 
+	name = models.CharField(max_length=None) # Name of user
+	rating = models.DecimalField(default=-1.0, max_digits=1, decimal_places=2) # rating of user
+	shoesOwned = models.ListTextField(base_field=models.IntegerField())
+
+class Transactions(models.Model):
+	transactionID = models.IntegerField(default=-1) # Transaction ID, uniqueID
+	pub_date = models.DateTimeField('transaction date') # Date of transaction
+	transactionType = models.IntegerField(default=0) # transaction type: sell
+	sellerID = models.IntegerField(default=-1) # sellerID
+	buyerID = models.IntegerField(default=-1) # buyerID
+
+class Inventory(models.Model):
+	shoeIndex = models.IntegerField(default=-1) # index number
+	quantity = models.IntegerField(default=-1)
+
+
+
