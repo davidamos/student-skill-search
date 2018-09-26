7bdf32539437864983417a1bf7eeddea8aff6e69
kmert10 <mertkarakas1997@gmail.com>
2018-09-26 01:09:09 -0400

    Added migrations to gitignore file

--- app/mmk2nh_cs4501/helloworld/models.py
+++ app/mmk2nh_cs4501/helloworld/models.py
@@ -1,21 +1,22 @@
 from django.db import models
+from django_mysql.models import ListTextField
 
 # Create your models here.
 class Shoe(models.Model):
 	index = models.IntegerField(default=-1) # index number, uniqueID
-	name = models.CharField(max_length=None) # Name of shoe
+	name = models.CharField(max_length=128) # Name of shoe
 	typeID = models.IntegerField(default=-1) # shoe type ID
-	brand = models.CharField(max_length=None) # Name of brand
-	price = models.DecimalField(default=-1.0, decimal_places=2)
+	brand = models.CharField(max_length=128) # Name of brand
+	price = models.DecimalField(default=-1.0, decimal_places=2, max_digits=10)
 	tradable = models.IntegerField(default=-1) # tradable flag
 	sellable = models.IntegerField(default=-1) # sellable flag
 	userID = models.IntegerField(default=-1) # userID -> sellers table
 
 class User(models.Model):
 	index = models.IntegerField(default=-1) # sellerID in 'Shoes' correspond to index number, uniqueID 
-	name = models.CharField(max_length=None) # Name of user
-	rating = models.DecimalField(default=-1.0, max_digits=1, decimal_places=2) # rating of user
-	shoesOwned = models.ListTextField(base_field=models.IntegerField())
+	name = models.CharField(max_length=128) # Name of user
+	rating = models.DecimalField(default=-1.0, max_digits=1, decimal_places=1) # rating of user
+	shoesOwned = ListTextField(base_field=models.IntegerField())
 
 class Transactions(models.Model):
 	transactionID = models.IntegerField(default=-1) # Transaction ID, uniqueID
