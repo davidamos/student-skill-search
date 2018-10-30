2bb65d59e4da08ed84c8ba4af99bf1feb6edf227
kmert10 <mertkarakas1997@gmail.com>
2018-10-30 12:15:49 -0400

    trying docker file

--- app/mmk2nh_cs4501/helloworld/tests.py
+++ app/mmk2nh_cs4501/helloworld/tests.py
@@ -1,6 +1,4 @@
 from django.test import TestCase
-from rest_framework.test import APIClient
-from rest_framework import status
 from django.urls import reverse
 from helloworld.models import Shoe, User, Transactions, Inventory
 
