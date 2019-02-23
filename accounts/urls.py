a7dd4c451631a8094ac9df2549612ec8be46883b
Richard Ohr <rfo8sf@virginia.edu>
2019-02-23 17:11:29 -0500

    added page to view own profile
    view is "profile", template is under accounts/templates/accounts/profile.html

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.SignUp.as_view(), name='signup'),
+    path('profile/', views.profile, name='profile')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,18 @@
 # accounts/views.py
 from django.urls import reverse_lazy
 from django.views import generic
+from django.shortcuts import redirect, render
 
 from .forms import CustomUserCreationForm
+from .models import CustomUser
 
 class SignUp(generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+def profile(request):
+    if request.user.is_authenticated:
+        return render(request, 'accounts/profile.html')
+    else:
+        return redirect("{% url 'login' %}")
