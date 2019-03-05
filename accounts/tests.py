033f754c8dd1bdb74de6e0bafc9e9a2e510af5db
PeterFelland <32556453+PeterFelland@users.noreply.github.com>
2019-03-04 20:07:00 -0500

    Added navbar to base.html + couple tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -11,3 +11,15 @@ class CustomUserModelTests(TestCase):
         x = 1
         y = 1
         self.assertEqual(x,y)
+
+    # Students are defaulted to searching for a partner
+    def test_is_searching(self):
+        student = CustomUser()
+        self.assertTrue(student.is_searching)
+
+    # Student checks that they are not searching
+    def test_not_searching(self):
+        student = CustomUser()
+        student.is_searching = False
+        self.assertEqual(student.is_searching, False)
+
