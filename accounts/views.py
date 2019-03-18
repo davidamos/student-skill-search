4be74e996a8b731b2ac3da6b69304abac62b1510
Kevin <kw2ae@virginia.edu>
2019-03-18 17:38:21 -0400

    edited to handle the case when courses are before 8am

--- accounts/views.py
+++ accounts/views.py
@@ -25,11 +25,16 @@ def post_course(request):
 			'''Need to check if start time is before end time'''
 			start_time = datetime.strptime(request.POST['course_start_time'], '%H:%M')
 			end_time = datetime.strptime(request.POST['course_end_time'], '%H:%M')
+			'''handle the case where end time is after noon and start time is before noon'''
 			if start_time < end_time:
 				course.course_start_time = start_time
 				course.course_end_time = end_time
 			else:
 				course.course_start_time = start_time
+				course.course_end_time = end_time + timedelta(hours=12)
+			'''handle case where time is too early to be an am class'''
+			if start_time < datetime.strptime('8:00', '%H:%M'):
+				course.course_start_time = start_time + timedelta(hours=12)
 				course.course_end_time = end_time + timedelta(hours=12)
 			course.save()
 			print(request.user)
