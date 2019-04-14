35b16d37bbdb3dcb8e0b576cd989115fbe9ed044
Richard Ohr <rfo8sf@virginia.edu>
2019-04-14 15:10:44 -0400

    added whitenoise to handle static files

--- student_skill_matching/settings.py
+++ student_skill_matching/settings.py
@@ -51,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     # social auth middleware
     'social_django.middleware.SocialAuthExceptionMiddleware',
 ]
