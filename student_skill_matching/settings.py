6a303b069b2d83a5ab2c9ac344cf9095f9cf78fd
Richard Ohr <rfo8sf@virginia.edu>
2019-04-29 13:30:50 -0400

    disabled debug

--- student_skill_matching/settings.py
+++ student_skill_matching/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'wikol+b7tr#)l3upuvcy^pp6&i=8f^_fm88t48k==8%ly5emvu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
